7e8ab11121da408ef0bed580a7c325b5a5a9878f
Chris Stanton <cstanton0760@yahoo.com>
2017-06-13 20:21:40 -0500

    added html and css to add user view

--- public/scripts/controllers/add_user.controller.js
+++ public/scripts/controllers/add_user.controller.js
@@ -1,9 +1,13 @@
 
-myApp.controller('Add_userController', function() {
-  console.log('Add_userController running');
-  var self = this;
-  self.message = 'angular sourced';
+  myApp.controller('Add_userController', function() {
+    console.log('Add_userController running');
+
+
+      var self = this;
+
+
+      self.message = 'angular sourced';
 
 
 
-}); // end controller code block
+    }); // end controller code block
