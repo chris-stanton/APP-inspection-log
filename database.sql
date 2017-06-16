151e0d7a40addbe067cf82070495a36078635e44
Chris Stanton <cstanton0760@yahoo.com>
2017-06-15 19:40:23 -0500

    created DB and database.sql file

--- database.sql
+++ database.sql
@@ -1,7 +1,33 @@
 
 
-CREATE TABLE table_one (
+CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(80) NOT NULL,
-	description VARCHAR(150) NOT NULL
+	username VARCHAR(100) NOT NULL unique,
+	password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE usersInfo (
+	id SERIAL PRIMARY KEY,
+	firstName VARCHAR(100) NOT NULL,
+	lastName VARCHAR(100) NOT NULL,
+	users_id INT NOT NULL,
+	companies_id INT NOT NULL,
+	active boolean DEFAULT true
+);
+
+CREATE TABLE companies (
+	id SERIAL PRIMARY KEY,
+	companyName VARCHAR(100) NOT NULL,
+	streetAddress VARCHAR(500) NOT NULL,
+	city VARCHAR(100) NOT NULL,
+	state VARCHAR(100) NOT NULL,
+	zipCode INT NOT NULL,
+	country VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE inspectionLogs (
+	id SERIAL PRIMARY KEY,
+	userInfo_id INT NOT NULL,
+	
+	active boolean DEFAULT true
 );
