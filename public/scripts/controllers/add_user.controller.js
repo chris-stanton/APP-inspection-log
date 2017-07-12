a868f602d19877fd007a97fdca41a275d9ba231b
Chris Stanton <cstanton0760@yahoo.com>
2017-07-11 23:03:25 -0500

    added else if to check if new user entered all data fields

--- public/scripts/controllers/add_user.controller.js
+++ public/scripts/controllers/add_user.controller.js
@@ -14,11 +14,30 @@
 // button click to add new user to DB
       self.addNewUser = function(newUser) {
         console.log('New User', newUser);
-        FactoryFactory.addNewUser(newUser);
-        // empties inputs after submit
-        self.newUser = {};
-        self.password = '';
-      }
+
+        if (newUser.firstName == null) {
+          console.log("enter first name");
+            alertify.alert("Please Enter First Name To Continue");
+        } else if (newUser.lastName == null) {
+          console.log("enter last name");
+            alertify.alert("Please Enter Last Name To Continue");
+        } else if (newUser.username == null) {
+          console.log("enter username");
+            alertify.alert("Please Enter Username To Continue");
+        } else if (newUser.password == null) {
+          console.log("enter password");
+            alertify.alert("Please Enter Password To Continue");
+        } else if (newUser.companyId == null) {
+          console.log("select company");
+            alertify.alert("Please Select Your Company To Continue");
+        } else {
+          FactoryFactory.addNewUser(newUser);
+          // empties inputs after submit
+          self.newUser = {};
+          self.password = '';
+        }
+
+      }; // end addNewUser()
 
 
     }]); // end controller code block
