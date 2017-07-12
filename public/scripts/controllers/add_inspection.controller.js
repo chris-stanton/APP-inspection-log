2a7aaf3b92953e43cdcd6fda830ff304092b8405
Chris Stanton <cstanton0760@yahoo.com>
2017-07-11 20:02:00 -0500

    cleaned up code in inspection form html one and aded comments

--- public/scripts/controllers/add_inspection.controller.js
+++ public/scripts/controllers/add_inspection.controller.js
@@ -1,15 +1,16 @@
 myApp.controller('Add_inspectionController',['FactoryFactory',function(FactoryFactory) {
 
-console.log('Add_inspectionController running');
+  console.log('Add_inspectionController running');
 
-var self = this;
-self.message = 'angular sourced';
+  var self = this;
+
+  self.message = 'angular sourced';
 
 
-self.submitInspectionLocation = function(inspectionLocation) {
-  console.log(inspectionLocation);
-  self.inspectionLocation = {};
-}; //end of addInspectionForm
+  self.submitInspectionLocation = function(inspectionLocation) {
+    console.log(inspectionLocation);
+    self.inspectionLocation = {};
+  }; //end of addInspectionForm
 
 
 }]); //end of myApp.controller
