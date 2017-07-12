cef1367b0f38c4192bbb72f3458811bf5ab3369a
Chris Stanton <cstanton0760@yahoo.com>
2017-07-11 20:03:35 -0500

    cleaned up html code

--- app.js
+++ app.js
@@ -8,19 +8,25 @@ var bodyParser = require('body-parser');
 var login = require('./server/routes/login.js');
 var init = require('./server/routes/init.js');
 
+
+
 //Serve back static middleware files
 app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 //Handle index file separately
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '.public/index.html'));
 })
 
+
 // routes
-app.use('/login', login);
-app.use('/init', init);
+app.use('/login', login); // route for all login views
+app.use('/init', init); // route for all views on init that does not need auth
+
 
 // port listening
 app.set('port', process.env.PORT || 5000);
