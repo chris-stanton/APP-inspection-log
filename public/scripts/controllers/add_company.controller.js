8f86507df8604ca832646e53ff58cfa3ce0218d9
Chris Stanton <cstanton0760@yahoo.com>
2017-07-11 23:24:01 -0500

    added comments to add company controller

--- public/scripts/controllers/add_company.controller.js
+++ public/scripts/controllers/add_company.controller.js
@@ -14,26 +14,26 @@ myApp.controller('Add_companyController',['FactoryFactory',function(FactoryFacto
 
     if (newCompany.companyName == null) {
       console.log("enter company name");
-        alertify.alert("Please Enter Company Name To Continue");
+        alertify.alert("Please Enter COMPANY NAME To Continue");
     } else if (newCompany.streetAddress == null) {
       console.log("enter street address");
-        alertify.alert("Please Enter Street Address To Continue");
+        alertify.alert("Please Enter STREET ADDRESS To Continue");
     } else if (newCompany.city == null) {
       console.log("enter city");
-        alertify.alert("Please Enter City To Continue");
+        alertify.alert("Please Enter CITY To Continue");
     } else if (newCompany.state == null) {
       console.log("enter state");
-        alertify.alert("Please Enter State To Continue");
+        alertify.alert("Please Enter STATE To Continue");
     } else if (newCompany.zipCode == null) {
       console.log("enter zip code");
-        alertify.alert("Please Enter Zip Code To Continue");
-    } else if (newCompany.contry == null) {
+        alertify.alert("Please Enter ZIP CODE To Continue");
+    } else if (newCompany.country == null) {
       console.log("enter contry");
-        alertify.alert("Please Enter Country To Continue");
+        alertify.alert("Please Enter COUNTRY To Continue");
     } else {
       // FactoryFactory.addNewCompany(newCompany);
       self.newCompany = {};
-    } // end of else if
+    } // end else if
 
   }; // end addNewCompany()
 
