a68ec7ae38ea150da51fee9fb8974cdff87331cf
Chris Stanton <cstanton0760@yahoo.com>
2017-07-11 23:05:16 -0500

    cleaned up code and added comments to add new user controller

--- public/scripts/controllers/add_user.controller.js
+++ public/scripts/controllers/add_user.controller.js
@@ -2,14 +2,13 @@
   myApp.controller('Add_userController', ['FactoryFactory', function(FactoryFactory) {
     console.log('Add_userController running');
 
-
       var self = this;
 
 // calls DB on init for all companies
       FactoryFactory.getAllCompanies();
-      self.allCompanies = FactoryFactory.allCompanies;
 
       self.message = 'angular sourced';
+      self.allCompanies = FactoryFactory.allCompanies;
 
 // button click to add new user to DB
       self.addNewUser = function(newUser) {
