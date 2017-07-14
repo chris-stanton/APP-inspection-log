b8f9282ebf25d5c8b9de20c898240b450eed9a01
Chris Stanton <cstanton0760@yahoo.com>
2017-07-14 13:16:08 -0500

    added stylin to chart on dashboard

--- public/scripts/controllers/dashboard.controller.js
+++ public/scripts/controllers/dashboard.controller.js
@@ -8,29 +8,19 @@ myApp.controller('DashboardController', function() {
   self.message = 'angular sourced';
 
   var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: '# of Inspection',
+            data: [12, 19, 3, 5, 2, 3, 6, 7, 8, 20, 11, 15],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                "transparent"
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                '#3777f1'
             ],
-            borderWidth: 1
+            borderWidth: 2
         }]
     },
     options: {
