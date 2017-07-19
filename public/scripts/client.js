142fb5fe9451d1a5ab599901e744a5b76c117c34
Chris Stanton <cstanton0760@yahoo.com>
2017-07-19 15:21:57 -0500

    added controllers for all inspection form views

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,6 @@ myApp.config(['$routeProvider', function($routeProvider) {
 
   //routes
     $routeProvider
-        .when ('/dashboard', {
-          templateUrl: '/views/dashboard.html',
-          controller: 'DashboardController',
-          controllerAs: 'dbc'
-        })
-        .when ('/add_inspection', {
-            templateUrl: '/views/add_inspection.html',
-            controller: 'Add_inspectionController',
-            controllerAs: 'aic'
-        })
-        .when ('/manage_inspections', {
-            templateUrl: '/views/manage_inspections.html',
-            controller: 'Manage_inspectionsController',
-            controllerAs: 'mic'
-        })
         .when ('/login', {
             templateUrl: '/views/login.html',
             controller: 'LoginController',
@@ -33,6 +18,51 @@ myApp.config(['$routeProvider', function($routeProvider) {
             templateUrl: '/views/add_company.html',
             controller: 'Add_companyController',
             controllerAs: 'acc'
+        })
+        .when ('/dashboard', {
+          templateUrl: '/views/dashboard.html',
+          controller: 'DashboardController',
+          controllerAs: 'dbc'
+        })
+        .when ('/add_inspection', {
+            templateUrl: '/views/add_inspection.html',
+            controller: 'Add_inspectionController',
+            controllerAs: 'aic'
+        })
+        .when ('/page_two', {
+            templateUrl: '/views/inspection_forms/page_two.html',
+            controller: 'Page_twoController',
+            controllerAs: 'two'
+        })
+        .when ('/page_three', {
+            templateUrl: '/views/inspection_forms/page_three.html',
+            controller: 'Page_threeController',
+            controllerAs: 'three'
+        })
+        .when ('/page_four', {
+            templateUrl: '/views/inspection_forms/page_four.html',
+            controller: 'Page_fourController',
+            controllerAs: 'four'
+        })
+        .when ('/page_five', {
+            templateUrl: '/views/inspection_forms/page_five.html',
+            controller: 'Page_fiveController',
+            controllerAs: 'five'
+        })
+        .when ('/page_six', {
+            templateUrl: '/views/inspection_forms/page_six.html',
+            controller: 'Page_sixController',
+            controllerAs: 'six'
+        })
+        .when ('/page_seven', {
+            templateUrl: '/views/inspection_forms/page_seven.html',
+            controller: 'Page_sevenController',
+            controllerAs: 'seven'
+        })
+        .when ('/manage_inspections', {
+            templateUrl: '/views/manage_inspections.html',
+            controller: 'Manage_inspectionsController',
+            controllerAs: 'mic'
         })
         .otherwise ( {
             redirectTo: '/login'
