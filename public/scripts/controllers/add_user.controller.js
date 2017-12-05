0a1702141478d67d1fbfa59962e2d5663eede944
Chris Stanton <cstanton0760@yahoo.com>
2017-12-04 23:38:36 -0600

    adedd error loging to login, add user, add company and dashborad controllers

--- public/scripts/controllers/add_user.controller.js
+++ public/scripts/controllers/add_user.controller.js
@@ -2,34 +2,34 @@
   myApp.controller('Add_userController', ['FactoryFactory', '$location', '$http', 'alertify', function(FactoryFactory, $location, $http, alertify) {
     console.log('Add_userController Running...');
 
-      var self = this;
+    var self = this;
 
 // calls DB on init for all companies
-      FactoryFactory.getAllCompanies();
+    FactoryFactory.getAllCompanies();
 
-      self.message = 'angular sourced';
+    self.message = 'angular sourced';
 // all companies form db
-      self.allCompanies = FactoryFactory.allCompanies;
+    self.allCompanies = FactoryFactory.allCompanies;
 
 // button click to add new user to DB
-      self.addNewUser = function(user) {
-        console.log('user', user);
-        if(self.user.username === '' || self.user.password === '') {
+    self.addNewUser = function(user) {
+      console.log('user', user);
+      if(self.user.username === '' || self.user.password === '') {
+        alertify.error("Missing Credentials! Please try again");
+        // self.error = "Missing Credentials! Please try again";
+      } else {
+        console.log('registerUser -- sending to server...', self.user);
+        $http.post('/register', self.user).then(function(response) {
+          console.log('registerUser -- success');
+          alertify.success('User profile was added to DB');
+          $location.path('/dashboard');
+        }).catch(function(response) {
           alertify.error("Missing Credentials! Please try again");
-          // self.error = "Missing Credentials! Please try again";
-        } else {
-          console.log('registerUser -- sending to server...', self.user);
-          $http.post('/register', self.user).then(function(response) {
-            console.log('registerUser -- success');
-            alertify.success('User profile was added to DB');
-            $location.path('/dashboard');
-          }).catch(function(response) {
-            alertify.error("Missing Credentials! Please try again");
-            // self.error = "Please try again."
-            console.log('registerUser -- error');
-          })
-        }
-      }; // end addNewUser()
+          // self.error = "Please try again."
+          console.log('registerUser -- error');
+        })
+      }
+    }; // end addNewUser()
 
 
-    }]); // end controller code block
+  }]); // end controller code block
